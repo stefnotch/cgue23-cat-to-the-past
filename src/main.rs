a106557a723bc7a7655bcc110fa17e39e4326280
YouSafe <yelagabani@hotmail.com>
2023-02-07 12:46:54 +0100

    Create Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use vulkano::VulkanLibrary;
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano_win::VkSurfaceBuild;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+
 fn main() {
-    println!("Hello, world");
+    let library = VulkanLibrary::new()
+        .expect("no local Vulkan library/DLL");
+
+    let instance_extensions = vulkano_win::required_extensions(&library);
+
+    let instance = Instance::new(library, InstanceCreateInfo {
+        enabled_extensions: instance_extensions,
+        ..Default::default()
+    }).expect("failed to create instance");
+
+    let event_loop = EventLoop::new();
+    let _surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance.clone())
+        .expect("could not create window");
+
+    event_loop.run(|event, _, control_flow| {
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+                *control_flow = ControlFlow::Exit
+            }
+            _ => ()
+        }
+    });
+
 }
