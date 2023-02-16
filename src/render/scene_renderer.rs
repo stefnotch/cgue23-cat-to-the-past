d9fa89205894f1c6acf3cfcc0a41c57d1f20cae6
YouSafe <yelagabani@hotmail.com>
2023-02-16 12:43:33 +0100

    Refactor

--- src/render/scene_renderer.rs
+++ src/render/scene_renderer.rs
@@ -1,24 +1,40 @@
+use crate::context::Context;
+use crate::mesh::{Mesh, MeshVertex};
+use std::default::Default;
 use std::sync::Arc;
-use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer, CpuBufferPool};
-use vulkano::command_buffer::{RenderPassBeginInfo, SubpassContents};
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer, CpuBufferPool, TypedBufferAccess};
+use vulkano::command_buffer::allocator::{
+    StandardCommandBufferAlloc, StandardCommandBufferAllocator,
+};
+use vulkano::command_buffer::{
+    AutoCommandBufferBuilder, CommandBufferExecFuture, CommandBufferUsage, RenderPassBeginInfo,
+    SubpassContents,
+};
 use vulkano::format::Format;
-use vulkano::image::ImageViewAbstract;
+use vulkano::image::view::ImageView;
+use vulkano::image::SwapchainImage;
 use vulkano::memory::allocator::{MemoryUsage, StandardMemoryAllocator};
 use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
 use vulkano::pipeline::graphics::vertex_input::BuffersDefinition;
-use vulkano::pipeline::graphics::viewport::ViewportState;
-use vulkano::pipeline::{GraphicsPipeline, PipelineBindPoint};
-use vulkano::render_pass::Subpass;
+use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
+use vulkano::pipeline::{GraphicsPipeline, Pipeline, PipelineBindPoint};
+use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass};
 use vulkano::sync::GpuFuture;
-use crate::context::Context;
-use crate::mesh::{Mesh, MeshVertex};
 
-pub struct SceneRenderer {}
+pub struct SceneRenderer {
+    pub render_pass: Arc<RenderPass>,
+    pub pipeline: Arc<GraphicsPipeline>,
+    pub mesh: Arc<Mesh>,
+    pub framebuffers: Vec<Arc<Framebuffer>>,
+    pub command_buffer_allocator: Arc<StandardCommandBufferAllocator>,
+}
 
 impl SceneRenderer {
     pub fn new(
         context: &Context,
+        images: &[Arc<ImageView<SwapchainImage>>],
         final_output_format: Format,
+        command_buffer_allocator: Arc<StandardCommandBufferAllocator>,
     ) -> Self {
         let vs = vs::load(context.device()).unwrap();
         let fs = fs::load(context.device()).unwrap();
@@ -51,7 +67,7 @@ impl SceneRenderer {
                 depth_stencil: {}
             }
         )
-            .unwrap();
+        .unwrap();
 
         let pipeline = GraphicsPipeline::start()
             .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
@@ -63,22 +79,62 @@ impl SceneRenderer {
             .build(context.device())
             .expect("could not create pipeline");
 
+        let framebuffers = images
+            .into_iter()
+            .map(|image| {
+                Framebuffer::new(
+                    render_pass.clone(),
+                    FramebufferCreateInfo {
+                        attachments: vec![image.clone()],
+                        ..FramebufferCreateInfo::default()
+                    },
+                )
+                .expect("failed to create framebuffer")
+            })
+            .collect();
+
         SceneRenderer {
             render_pass,
             pipeline,
-            mesh: cube
+            mesh: cube,
+            framebuffers,
+            command_buffer_allocator,
         }
     }
 
-    pub fn resize(&self) {}
+    pub fn resize(&mut self, images: &[Arc<ImageView<SwapchainImage>>]) {
+        self.framebuffers = images
+            .into_iter()
+            .map(|image| {
+                Framebuffer::new(
+                    self.render_pass.clone(),
+                    FramebufferCreateInfo {
+                        attachments: vec![image.clone()],
+                        ..FramebufferCreateInfo::default()
+                    },
+                )
+                .expect("failed to create framebuffer")
+            })
+            .collect();
+    }
 
     pub fn render<F>(
         &self,
+        context: &Context,
         future: F,
-        final_image: Arc<dyn ImageViewAbstract<Handle=()> + 'static>,
-    )
-        where F: GpuFuture + 'static
+        swapchain_frame_index: u32,
+        viewport: &Viewport,
+    ) -> CommandBufferExecFuture<F>
+    where
+        F: GpuFuture + 'static,
     {
+        let mut builder = AutoCommandBufferBuilder::primary(
+            &self.command_buffer_allocator,
+            context.queue_family_index(),
+            CommandBufferUsage::OneTimeSubmit,
+        )
+        .unwrap();
+
         builder
             // Before we can draw, we have to *enter a render pass*.
             .begin_render_pass(
@@ -90,7 +146,9 @@ impl SceneRenderer {
                     // Only attachments that have `LoadOp::Clear` are provided with clear
                     // values, any others should use `ClearValue::None` as the clear value.
                     clear_values: vec![Some([0.0, 0.0, 1.0, 1.0].into())],
-                    ..RenderPassBeginInfo::framebuffer(final_image)
+                    ..RenderPassBeginInfo::framebuffer(
+                        self.framebuffers[swapchain_frame_index as usize].clone(),
+                    )
                 },
                 SubpassContents::Inline,
             )
@@ -100,17 +158,26 @@ impl SceneRenderer {
             // The last two parameters contain the list of resources to pass to the shaders.
             // Since we used an `EmptyPipeline` object, the objects have to be `()`.
             .set_viewport(0, [viewport.clone()])
-            .bind_pipeline_graphics(pipeline.clone())
-            .bind_descriptor_sets(PipelineBindPoint::Graphics, pipeline.layout().clone(), 0, set.clone())
-            .bind_index_buffer(index_buffer.clone())
-            .bind_vertex_buffers(0, vertex_buffer.clone())
-            .draw_indexed(index_buffer.len() as u32, 1, 0, 0, 0)
+            .bind_pipeline_graphics(self.pipeline.clone())
+            .bind_descriptor_sets(
+                PipelineBindPoint::Graphics,
+                self.pipeline.layout().clone(),
+                0,
+                set.clone(),
+            )
+            .bind_index_buffer(self.mesh.index_buffer.clone())
+            .bind_vertex_buffers(0, self.mesh.vertex_buffer.clone())
+            .draw_indexed(self.mesh.index_buffer.len() as u32, 1, 0, 0, 0)
             .unwrap()
             .end_render_pass()
             .unwrap();
 
         // Finish building the command buffer by calling `build`.
         let command_buffer = builder.build().unwrap();
+
+        future
+            .then_execute(context.queue(), command_buffer)
+            .unwrap()
     }
 }
 
