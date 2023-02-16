6e321774a522eda0c66efa6cf3c62bf524429180
stefnotch <brandmairstefan@gmail.com>
2023-02-16 16:10:13 +0100

    Add basic scene graph

---
+++ src/scene/scene_graph.rs
@@ -0,0 +1,187 @@
+use cgmath::{Quaternion, Vector2, Vector3, Zero};
+
+use super::mesh::Mesh;
+
+struct SceneGraph {
+    root: SceneNode,
+}
+
+struct SceneNode {
+    transform: Transform,
+    bounding_box: BoundingBox,
+    data: SceneNodeData,
+    children: Vec<SceneNode>,
+}
+
+trait ToSceneNodeData {
+    /// for inserting
+    fn to_scene_node_data(self) -> SceneNodeData;
+}
+trait FromSceneNodeData {
+    /// for getting
+    fn from_scene_node_data(value: &SceneNodeData) -> Option<&Self>
+    where
+        Self: Sized;
+}
+
+enum SceneNodeData {
+    Model(Model),
+    RigidBody(RigidBody),
+    Light(Light),
+    Empty,
+}
+
+struct Transform {
+    position: Vector3<f32>,
+    rotation: Quaternion<f32>,
+    scale: Vector3<f32>,
+}
+impl Transform {
+    fn new() -> Transform {
+        Transform {
+            position: Vector3::zero(),
+            rotation: Quaternion::new(1.0, 0.0, 0.0, 0.0),
+            scale: Vector3::new(1.0, 1.0, 1.0),
+        }
+    }
+}
+
+/// Axis-aligned bounding box.
+struct BoundingBox {
+    min: Vector3<f32>,
+    max: Vector3<f32>,
+}
+impl BoundingBox {
+    fn new() -> BoundingBox {
+        BoundingBox {
+            min: Vector3::zero(),
+            max: Vector3::zero(),
+        }
+    }
+}
+
+struct Model {
+    geometry: Mesh,
+    material: (), //Material, // references a shader and its inputs
+}
+
+struct RigidBody {
+    // physics engine stuff
+}
+
+struct Light {
+    color: Vector3<f32>,
+    intensity: f32,
+}
+
+impl SceneGraph {
+    fn new() -> Self {
+        Self {
+            root: SceneNode::new(),
+        }
+    }
+
+    fn get_data_recursive<'a, T>(&'a self) -> Vec<&'a T>
+    where
+        T: FromSceneNodeData,
+    {
+        let mut result = Vec::new();
+        {
+            self.root.get_data_recursive(&mut result);
+        }
+        result
+    }
+}
+
+impl SceneNode {
+    fn new() -> Self {
+        Self {
+            transform: Transform::new(),
+            bounding_box: BoundingBox::new(),
+            data: SceneNodeData::Empty,
+            children: Vec::new(),
+        }
+    }
+
+    // TODO: Figure out why the lifetime annotations work like that
+    fn get_data_recursive<'a, 'b, T>(&'a self, mut result: &'b mut Vec<&'a T>)
+    where
+        T: FromSceneNodeData,
+    {
+        if let Some(value) = T::from_scene_node_data(&self.data) {
+            result.push(value);
+        }
+
+        self.children.iter().for_each(|child| {
+            child.get_data_recursive(&mut result);
+        });
+    }
+}
+
+impl ToSceneNodeData for Model {
+    fn to_scene_node_data(self) -> SceneNodeData {
+        SceneNodeData::Model(self)
+    }
+}
+
+impl FromSceneNodeData for Model {
+    fn from_scene_node_data(value: &SceneNodeData) -> Option<&Self> {
+        match value {
+            SceneNodeData::Model(v) => Some(v),
+            _ => None,
+        }
+    }
+}
+
+/*
+
+
+#[derive(Debug)]
+struct Foo;
+#[derive(Debug)]
+struct Bar;
+
+#[derive(Debug)]
+enum MyEnum {
+    Foos(Foo),
+    Bars(Bar),
+}
+
+trait FromMyEnum {
+    fn from_enum(value: &MyEnum) -> Option<&Self>
+    where Self: Sized;
+}
+impl FromMyEnum for Foo {
+    fn from_enum(value: &MyEnum) -> Option<&Self> {
+        match value {
+            MyEnum::Foos(v) => Some(&v),
+            _ => None
+        }
+    }
+
+}
+#[derive(Debug)]
+struct Cont {
+    values: Vec<MyEnum>
+}
+impl Cont {
+    fn get_all<T>(&self) -> Vec<&T> where T : FromMyEnum {
+        let mut result = Vec::new();
+        self.values.iter().for_each(|child| {
+            if let Some(value) = T::from_enum(child) {
+                result.push(value);
+            }
+        });
+        result
+    }
+
+}
+
+fn main() {
+    let e = Cont{values: vec![MyEnum::Foos(Foo), MyEnum::Bars(Bar), MyEnum::Foos(Foo)]};
+    let ee = e.get_all::<Foo>();
+    let ee2 = e.get_all::<Bar>();
+    //dbg!(e);
+    dbg!(ee);
+}
+ */
