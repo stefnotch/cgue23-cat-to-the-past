d7fc683d2ab69cb3eb9b9b99cdebb2e4ce4248c3
stefnotch <brandmairstefan@gmail.com>
2023-02-16 14:54:52 +0100

    Refactpor swapchain

--- src/render/main_renderer.rs
+++ src/render/main_renderer.rs
@@ -18,31 +18,30 @@ use winit::window::Window;
 /// Responsible for keeping the swapchain up-to-date and calling the sub-rendersystems
 pub struct Renderer {
     recreate_swapchain: bool,
+    // TODO: Huh, this doesn't need to be an option?
     previous_frame_end: Option<Box<dyn GpuFuture>>,
-    swapchain: Arc<Swapchain>,
-    images: Vec<Arc<ImageView<SwapchainImage>>>,
+    swapchain: SwapchainContainer,
     scene_renderer: SceneRenderer,
     viewport: Viewport,
+}
+
+struct SwapchainContainer {
+    swapchain: Arc<Swapchain>,
+    images: Vec<Arc<ImageView<SwapchainImage>>>,
+    dimensions: [u32; 2],
 }
 
 impl Renderer {
     pub fn new(context: &Context) -> Renderer {
         let previous_frame_end = Some(sync::now(context.device()).boxed());
 
-        let (swapchain, images) = create_swapchain(context.device(), context.surface());
-
-        let dimensions = images[0].dimensions().width_height().map(|i| i as f32);
+        let swapchain = SwapchainContainer::new(context.device(), context.surface());
 
         let viewport = Viewport {
             origin: [0.0, 0.0],
-            dimensions,
+            dimensions: swapchain.dimensions.map(|i| i as f32),
             depth_range: 0.0..1.0,
         };
-
-        let images = images
-            .into_iter()
-            .map(|image| ImageView::new_default(image.clone()).unwrap())
-            .collect::<Vec<_>>();
 
         let command_buffer_allocator = Arc::new(StandardCommandBufferAllocator::new(
             context.device(),
@@ -51,8 +50,8 @@ impl Renderer {
 
         let scene_renderer = SceneRenderer::new(
             context,
-            &images,
-            swapchain.image_format(),
+            &swapchain.images,
+            swapchain.swapchain.image_format(),
             command_buffer_allocator,
         );
 
@@ -60,7 +59,6 @@ impl Renderer {
             recreate_swapchain: false,
             previous_frame_end,
             swapchain,
-            images,
             scene_renderer,
             viewport,
         }
@@ -89,31 +87,19 @@ impl Renderer {
         // In this example that includes the swapchain, the framebuffers and the dynamic state viewport.
         if self.recreate_swapchain {
             // Use the new dimensions of the window.
-
-            let (new_swapchain, new_images) = match self.swapchain.recreate(SwapchainCreateInfo {
-                image_extent: dimensions.into(),
-                ..self.swapchain.create_info()
-            }) {
+            match self.swapchain.recreate() {
                 Ok(r) => r,
                 // This error tends to happen when the user is manually resizing the window.
                 // Simply restarting the loop is the easiest way to fix this issue.
                 Err(SwapchainCreationError::ImageExtentNotSupported { .. }) => return,
                 Err(e) => panic!("Failed to recreate swapchain: {e:?}"),
-            };
+            }
 
-            self.swapchain = new_swapchain;
-
-            let dimensions = new_images[0].dimensions().width_height().map(|i| i as f32);
-            self.viewport.dimensions = dimensions;
-
-            self.images = new_images
-                .into_iter()
-                .map(|image| ImageView::new_default(image.clone()).unwrap())
-                .collect::<Vec<_>>();
+            self.viewport.dimensions = self.swapchain.dimensions.map(|i| i as f32);
 
             // TODO: delegate task to fetch new framebuffers to subrendersystems
 
-            self.scene_renderer.resize(&self.images);
+            self.scene_renderer.resize(&self.swapchain.images);
 
             self.recreate_swapchain = false;
         }
@@ -126,7 +112,7 @@ impl Renderer {
         // This function can block if no image is available. The parameter is an optional timeout
         // after which the function call will return an error.
         let (image_index, suboptimal, acquire_future) =
-            match acquire_next_image(self.swapchain.clone(), None) {
+            match acquire_next_image(self.swapchain.swapchain.clone(), None) {
                 Ok(r) => r,
                 Err(AcquireError::OutOfDate) => {
                     self.recreate_swapchain = true;
@@ -152,7 +138,10 @@ impl Renderer {
         let future = future
             .then_swapchain_present(
                 context.queue(),
-                SwapchainPresentInfo::swapchain_image_index(self.swapchain.clone(), image_index),
+                SwapchainPresentInfo::swapchain_image_index(
+                    self.swapchain.swapchain.clone(),
+                    image_index,
+                ),
             )
             .then_signal_fence_and_flush();
 
@@ -172,57 +161,86 @@ impl Renderer {
     }
 }
 
-pub fn create_swapchain(
-    device: Arc<Device>,
-    surface: Arc<Surface>,
-) -> (Arc<Swapchain>, Vec<Arc<SwapchainImage>>) {
-    let (swapchain, images) = {
-        let surface_capabilities = device
-            .physical_device()
-            .surface_capabilities(&surface, SurfaceInfo::default())
-            .expect("could not fetch surface capabilities");
-
-        let image_format = Some(
-            device
+impl SwapchainContainer {
+    pub fn new(device: Arc<Device>, surface: Arc<Surface>) -> SwapchainContainer {
+        let (swapchain, images) = {
+            let surface_capabilities = device
                 .physical_device()
-                .surface_formats(&surface, SurfaceInfo::default())
-                .expect("could not fetch surface formats")
-                .iter()
-                .min_by_key(|(format, color)| {
-                    // Prefer a RGB8/sRGB format
-                    match (format, color) {
-                        (Format::B8G8R8A8_SRGB, _) => 1,
-                        (Format::R8G8B8A8_SRGB, ColorSpace::SrgbNonLinear) => 2,
-                        (_, _) => 3,
-                    }
-                })
-                .expect("could not fetch image format")
-                .0, // just the format
-        );
+                .surface_capabilities(&surface, SurfaceInfo::default())
+                .expect("could not fetch surface capabilities");
 
-        let window = surface.object().unwrap().downcast_ref::<Window>().unwrap();
+            let image_format = Some(
+                device
+                    .physical_device()
+                    .surface_formats(&surface, SurfaceInfo::default())
+                    .expect("could not fetch surface formats")
+                    .iter()
+                    .min_by_key(|(format, color)| {
+                        // Prefer a RGB8/sRGB format
+                        match (format, color) {
+                            (Format::B8G8R8A8_SRGB, _) => 1,
+                            (Format::R8G8B8A8_SRGB, ColorSpace::SrgbNonLinear) => 2,
+                            (_, _) => 3,
+                        }
+                    })
+                    .expect("could not fetch image format")
+                    .0, // just the format
+            );
 
-        Swapchain::new(
-            device.clone(),
-            surface.clone(),
-            SwapchainCreateInfo {
-                min_image_count: surface_capabilities.min_image_count + 1,
-                image_format,
-                image_extent: window.inner_size().into(),
-                image_usage: ImageUsage {
-                    color_attachment: true,
-                    // TODO: depth attachment needed?
+            let window = surface.object().unwrap().downcast_ref::<Window>().unwrap();
+
+            Swapchain::new(
+                device.clone(),
+                surface.clone(),
+                SwapchainCreateInfo {
+                    min_image_count: surface_capabilities.min_image_count + 1,
+                    image_format,
+                    image_extent: window.inner_size().into(),
+                    image_usage: ImageUsage {
+                        color_attachment: true,
+                        // TODO: depth attachment needed?
+                        ..Default::default()
+                    },
+                    composite_alpha: surface_capabilities
+                        .supported_composite_alpha
+                        .iter()
+                        .next()
+                        .expect("could not fetch supported composite alpha"),
                     ..Default::default()
                 },
-                composite_alpha: surface_capabilities
-                    .supported_composite_alpha
-                    .iter()
-                    .next()
-                    .expect("could not fetch supported composite alpha"),
-                ..Default::default()
-            },
-        )
-        .expect("failed to create swapchain")
-    };
-    (swapchain, images)
+            )
+            .expect("failed to create swapchain")
+        };
+
+        let dimensions = images[0].dimensions().width_height();
+
+        let images = images
+            .into_iter()
+            .map(|image| ImageView::new_default(image.clone()).unwrap())
+            .collect::<Vec<_>>();
+
+        SwapchainContainer {
+            swapchain,
+            images,
+            dimensions,
+        }
+    }
+
+    fn recreate(&mut self) -> Result<(), SwapchainCreationError> {
+        match self.swapchain.recreate(SwapchainCreateInfo {
+            image_extent: self.dimensions.into(),
+            ..self.swapchain.create_info()
+        }) {
+            Ok((new_swapchain, new_images)) => {
+                self.swapchain = new_swapchain;
+                self.dimensions = new_images[0].dimensions().width_height();
+                self.images = new_images
+                    .into_iter()
+                    .map(|image| ImageView::new_default(image.clone()).unwrap())
+                    .collect::<Vec<_>>();
+                Ok(())
+            }
+            Err(v) => Err(v),
+        }
+    }
 }
