50d5787bde0857b6d6b16c08dbc52d4734f685c5
stefnotch <brandmairstefan@gmail.com>
2023-02-18 09:49:03 +0100

    Keyboard input upgrade

--- src/input.rs
+++ src/input.rs
@@ -4,6 +4,7 @@ const NUM_KEYS: usize = VirtualKeyCode::Cut as usize + 1;
 
 pub struct InputMap {
     state: [bool; NUM_KEYS],
+    last_state: [bool; NUM_KEYS],
     /// delta since last frame
     mouse_delta: (f64, f64),
 }
@@ -12,6 +13,7 @@ impl InputMap {
     pub fn new() -> Self {
         InputMap {
             state: [false; NUM_KEYS],
+            last_state: [false; NUM_KEYS],
             mouse_delta: (0.0, 0.0),
         }
     }
@@ -28,6 +30,14 @@ impl InputMap {
         self.state[key as usize]
     }
 
+    pub fn is_pressed_this_frame(&self, key: VirtualKeyCode) -> bool {
+        self.state[key as usize] && !self.last_state[key as usize]
+    }
+
+    pub fn is_released_this_frame(&self, key: VirtualKeyCode) -> bool {
+        !self.state[key as usize] && self.last_state[key as usize]
+    }
+
     pub fn mouse_move(&mut self, delta: (f64, f64)) {
         self.mouse_delta.0 += delta.0;
         self.mouse_delta.1 += delta.1;
@@ -39,5 +49,6 @@ impl InputMap {
 
     pub fn new_frame(&mut self) {
         self.mouse_delta = (0.0, 0.0);
+        self.last_state.copy_from_slice(&self.state[..]);
     }
 }
