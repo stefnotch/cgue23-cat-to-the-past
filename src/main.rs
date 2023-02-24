0ec58e413f096bdf2b1342986e636250075a940b
YouSafe <yelagabani@hotmail.com>
2023-02-24 12:17:41 +0100

    Add PlayerController

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::application::{AppConfig, Application, GameState, Run};
+use crate::camera::PlayerController;
 use cgmath::Rad;
 
 mod application;
@@ -9,10 +10,14 @@ mod physics;
 mod render;
 mod scene;
 
-struct Game {}
+struct Game {
+    player_controller: PlayerController,
+}
 
 impl Run for Game {
     fn init(&self, state: &mut GameState) {
+        // setup scene
+
         /*let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(context.device()));
         let cube = Mesh::cube(0.5, 0.5, 0.5, &memory_allocator);
 
@@ -21,16 +26,18 @@ impl Run for Game {
 
     fn input(&self, _state: &mut GameState) {}
 
-    fn update(&self, state: &mut GameState, _delta_time: f64) {
-        let (dx, dy) = state.input_map.mouse_delta();
-        state.camera.yaw += Rad(dx as f32 * 0.005);
-        state.camera.pitch += Rad(dy as f32 * 0.005);
+    fn update(&mut self, state: &mut GameState, delta_time: f64) {
+        self.player_controller
+            .update_camera(&mut state.camera, &state.input_map, delta_time);
     }
 }
 
 impl Game {
     pub fn new() -> Game {
-        Game {}
+        let controller = PlayerController::new(5.0, 0.5);
+        Game {
+            player_controller: controller,
+        }
     }
 }
 
@@ -40,10 +47,11 @@ fn main() {
         resolution: (800, 800),
         fullscreen: false,
         brightness: 1.0,
-        refresh_rate: 60, // ignored for now
+        refresh_rate: 60,
     };
 
-    let game = Game::new();
     let application = Application::new(&config);
+
+    let game = Game::new();
     application.run(game);
 }
