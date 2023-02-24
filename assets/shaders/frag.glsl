3c20bc413775b3c6078fd083c477edaf6d752e34
YouSafe <yelagabani@hotmail.com>
2023-02-24 12:46:56 +0100

    Add depth buffer + normals

--- assets/shaders/frag.glsl
+++ assets/shaders/frag.glsl
@@ -1,5 +1,17 @@
 #version 450
+
+layout(location = 0) in vec3 v_normal;
+
 layout(location = 0) out vec4 f_color;
+
+const vec3 light_dir = vec3(0.2f, -1.0f, 0.3f);
+const vec3 light_color = vec3(1.0f);
+
 void main() {
-    f_color = vec4(1.0, 0.0, 0.0, 1.0);
+    vec3 l = -normalize(light_dir);
+    vec3 n = v_normal;
+
+    vec3 diffuse = max(dot(n, l), 0) * light_color;
+
+    f_color = vec4(diffuse, 1.0);
 }
