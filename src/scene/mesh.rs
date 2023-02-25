27298b7a4da5062392ef800e74a45feaa975fab6
stefnotch <brandmairstefan@gmail.com>
2023-02-25 12:57:40 +0100

    Fix cube winding order

--- src/scene/mesh.rs
+++ src/scene/mesh.rs
@@ -35,15 +35,15 @@ impl Mesh {
 
         let positions: [Vector3<f32>; 8] = [
             // front
-            Vector3::new(-0.5, -0.5, 0.5),
-            Vector3::new(0.5, -0.5, 0.5),
-            Vector3::new(0.5, 0.5, 0.5),
-            Vector3::new(-0.5, 0.5, 0.5),
-            // back
             Vector3::new(-0.5, -0.5, -0.5),
             Vector3::new(0.5, -0.5, -0.5),
             Vector3::new(0.5, 0.5, -0.5),
             Vector3::new(-0.5, 0.5, -0.5),
+            // back
+            Vector3::new(-0.5, -0.5, 0.5),
+            Vector3::new(0.5, -0.5, 0.5),
+            Vector3::new(0.5, 0.5, 0.5),
+            Vector3::new(-0.5, 0.5, 0.5),
         ];
 
         let faces: [CubeFace; 6] = [
