95862af6b524b4f37864a027f4c2c03b9798cc12
stefnotch <brandmairstefan@gmail.com>
2023-02-28 17:41:28 +0100

    finish ecs migration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::sync::Arc;
+
+use bevy_ecs::system::{Commands, Res};
+use context::Context;
+use scene::material::Material;
+use scene::mesh::Mesh;
+use scene::model::Model;
+use scene::transform::Transform;
+
 use crate::application::{AppConfig, AppStage, ApplicationBuilder};
 use crate::player::PlayerSettings;
 
@@ -11,12 +20,32 @@ mod render;
 mod scene;
 mod time;
 
-fn hello_world() {
-    println!("Hello world");
-}
+fn spawn_world(mut commands: Commands, context: Res<Context>) {
+    let memory_allocator = Arc::new(
+        vulkano::memory::allocator::StandardMemoryAllocator::new_default(context.device()),
+    );
 
-fn hello_stefan() {
-    println!("Hallo Stefan!");
+    let cube = Mesh::cube(0.5, 0.5, 0.5, &memory_allocator);
+
+    let plane = Mesh::plane_horizontal(5.0, 5.0, &memory_allocator);
+    commands.spawn((
+        Model {
+            mesh: cube,
+            material: Arc::new(Material {}),
+        },
+        Transform::new(),
+    ));
+
+    let mut plane_transform = Transform::new();
+    plane_transform.position = cgmath::Vector3::new(0.0, -0.5, 0.0);
+
+    commands.spawn((
+        Model {
+            mesh: plane,
+            material: Arc::new(Material {}),
+        },
+        plane_transform,
+    ));
 }
 
 fn main() {
@@ -34,8 +63,7 @@ fn main() {
     let player_settings = PlayerSettings::new(5.0, 0.5);
 
     let application = ApplicationBuilder::new(config)
-        .with_startup_system(hello_world)
-        .with_system(AppStage::Update, hello_stefan)
+        .with_startup_system(spawn_world)
         .with_player_controller(player_settings)
         .build();
 
