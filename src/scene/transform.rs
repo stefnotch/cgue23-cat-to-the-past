cdf4668167c581df77a3a8d92449ef5007e2eb18
stefnotch <brandmairstefan@gmail.com>
2023-03-07 22:12:22 +0100

    Yay physics

--- src/scene/transform.rs
+++ src/scene/transform.rs
@@ -1,5 +1,6 @@
 use bevy_ecs::prelude::*;
 use cgmath::{Matrix4, Quaternion, Vector3, Zero};
+use rapier3d::na::{Isometry3, Translation3, UnitQuaternion};
 
 #[derive(Component)]
 pub struct Transform {
@@ -20,5 +21,19 @@ impl Transform {
         Matrix4::from_translation(self.position)
             * Matrix4::from(self.rotation)
             * Matrix4::from_nonuniform_scale(self.scale.x, self.scale.y, self.scale.z)
+    }
+
+    pub fn to_isometry(
+        &self,
+    ) -> rapier3d::na::Isometry<f32, rapier3d::na::Unit<rapier3d::na::Quaternion<f32>>, 3> {
+        Isometry3::from_parts(
+            Translation3::new(self.position.x, self.position.y, self.position.z),
+            UnitQuaternion::from_quaternion(rapier3d::na::Quaternion::new(
+                self.rotation.s,
+                self.rotation.v.x,
+                self.rotation.v.y,
+                self.rotation.v.z,
+            )),
+        )
     }
 }
