26916c62270c213c82e3e5f739d132bc0f7faadb
stefnotch <brandmairstefan@gmail.com>
2023-04-08 11:09:25 +0200

    WIP Time Rewinding

--- src/core/time_manager/game_change.rs
+++ src/core/time_manager/game_change.rs
@@ -1,12 +1,152 @@
-pub trait GameChange
+use std::collections::HashMap;
+
+use bevy_ecs::world::EntityMut;
+
+use super::level_time::LevelTime;
+
+pub trait GameState
 where
     Self: Sync + Send,
 {
-    //fn apply(&self, )
+    // Unsure about this one vv
+    fn skip_during_rewind(&self) -> bool;
 
-    // TODO: Properly think this compression through!
-    // dyn trait is interesting https://doc.rust-lang.org/error_codes/E0038.html#method-references-the-self-type-in-its-parameters-or-return-type
-    fn is_similar(&self, other: &Self) -> bool
+    fn interpolate(&self, other: &Self, t: f32) -> Self
     where
         Self: Sized;
+
+    // Alternate design: this could fire an event which a system picks up on?
+    // https://docs.rs/bevy_ecs/latest/bevy_ecs/world/struct.World.html#method.get_entity_mut
+    fn apply(&self, entity: &mut EntityMut);
+}
+
+pub(super) struct SingleFrameGameChanges {
+    timestamp: LevelTime,
+    changes: HashMap<uuid::Uuid, GameStateRange>,
+}
+
+/// Collections of game changes over time
+pub(super) struct GameChanges {
+    // Requirements
+    // - fast UUID lookups
+    // - don't store the timestamp twice for different UUIDs
+    // - don't store the UUID twice for different timestamps
+    // - fast insertion
+    timestamps: Vec<LevelTime>,
+    changes: HashMap<uuid::Uuid, GameStateRange>,
+}
+
+pub(super) enum StateLookup {
+    Nearest(LevelTime),
+    Interpolated(LevelTime),
+}
+
+impl StateLookup {
+    pub fn time(&self) -> LevelTime {
+        match self {
+            StateLookup::Nearest(time) => *time,
+            StateLookup::Interpolated(time) => *time,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+struct LevelTimeIndex(usize);
+
+/// An object with a unique ID lives for a certain amount of time, and has a state at each point in time
+pub(super) struct GameStateRange {
+    start_time: LevelTimeIndex,
+    // TODO: Add a lifetime here?
+    states: Vec<Box<dyn GameState>>,
+}
+
+impl GameChanges {
+    pub fn new() -> Self {
+        Self {
+            timestamps: Vec::new(),
+            changes: HashMap::new(),
+        }
+    }
+
+    pub fn add_all(&mut self, changes: SingleFrameGameChanges) {
+        let timestamp_index = LevelTimeIndex(self.timestamps.len());
+        self.timestamps.push(changes.timestamp);
+
+        for (id, new_state_range) in changes.changes {
+            let state_range = self.get_or_create_state_range(id, timestamp_index);
+
+            state_range.states.extend(new_state_range.states);
+        }
+    }
+
+    fn get_or_create_state_range(
+        &mut self,
+        id: uuid::Uuid,
+        timestamp_index: LevelTimeIndex,
+    ) -> &mut GameStateRange {
+        self.changes.entry(id).or_insert_with(|| GameStateRange {
+            start_time: timestamp_index,
+            states: Vec::new(),
+        })
+    }
+
+    pub fn clear(&mut self) {
+        self.timestamps.clear();
+        self.changes.clear();
+    }
+
+    pub fn apply(&self, level_time_lookup: StateLookup) {
+        if self.timestamps.is_empty() {
+            return;
+        }
+
+        let (time_start_index, time_end) = self.get_time_index_range(level_time_lookup);
+    }
+
+    fn get_time_index_range(
+        &self,
+        level_time_lookup: StateLookup,
+    ) -> (usize, Option<(usize, f32)>) {
+        if self.timestamps.len() == 1 {
+            (0, None)
+        } else {
+            match self.timestamps.binary_search(&level_time_lookup.time()) {
+                Ok(time_index) => (time_index, None),
+                Err(time_index) => {
+                    if time_index == self.timestamps.len() {
+                        (time_index - 1, None)
+                    } else if time_index == 0 {
+                        (time_index, None)
+                    } else {
+                        (time_index - 1, Some((time_index, 0.0)))
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl SingleFrameGameChanges {
+    pub fn new() -> Self {
+        Self {
+            timestamp: LevelTime::zero(),
+            changes: HashMap::new(),
+        }
+    }
+
+    pub fn set_timestamp(&mut self, timestamp: LevelTime) {
+        self.timestamp = timestamp;
+    }
+
+    pub fn add_state<T>(&mut self, id: uuid::Uuid, state: T)
+    where
+        T: GameState + 'static, // TODO: Seems like the wrong lifetime
+    {
+        let state_range = self.changes.entry(id).or_insert_with(|| GameStateRange {
+            start_time: LevelTimeIndex(0),
+            states: Vec::new(),
+        });
+
+        state_range.states.push(Box::new(state));
+    }
 }
--- src/physics/physics_change.rs
+++ src/physics/physics_change.rs
@@ -1,53 +1,86 @@
 use nalgebra::Vector3;
 use rapier3d::prelude::RigidBodyType;
 
-use crate::core::time_manager::{game_change::GameChange, TimeTracked};
+use crate::core::time_manager::game_change::GameState;
 
-pub(super) struct VelocityChange {
-    id: uuid::Uuid,
+use super::physics_context::{PhysicsContext, RapierRigidBodyHandle};
+
+pub(super) struct VelocityState {
     linvel: Vector3<f32>,
     angvel: Vector3<f32>,
 }
 
-impl VelocityChange {
-    pub fn new(time_tracked: &TimeTracked, linvel: Vector3<f32>, angvel: Vector3<f32>) -> Self {
-        Self {
-            id: time_tracked.id(),
-            linvel,
-            angvel,
-        }
+impl VelocityState {
+    pub fn new(linvel: Vector3<f32>, angvel: Vector3<f32>) -> Self {
+        Self { linvel, angvel }
     }
 }
 
-impl GameChange for VelocityChange {
-    fn is_similar(&self, other: &Self) -> bool
+impl GameState for VelocityState {
+    fn interpolate(&self, other: &Self, t: f32) -> Self
     where
         Self: Sized,
     {
-        // TODO: check if the velocity is on the LERP path...
-        self.id == other.id && self.linvel == other.linvel && self.angvel == other.angvel
+        Self {
+            linvel: other.linvel.lerp(&self.linvel, t),
+            angvel: other.angvel.lerp(&self.angvel, t),
+        }
+    }
+
+    fn apply(&self, entity: &mut bevy_ecs::world::EntityMut) {
+        todo!()
+        /* let rigid_body_handle = entity.get::<RapierRigidBodyHandle>().unwrap();
+
+        let mut physics_context = entity.world().resource_mut::<PhysicsContext>();
+        let mut rigidbody = physics_context
+            .rigid_bodies
+            .get_mut(rigid_body_handle.handle)
+            .unwrap();
+
+        rigidbody.set_linvel(self.linvel, false);
+        rigidbody.set_angvel(self.angvel, true); */
+    }
+
+    fn skip_during_rewind(&self) -> bool {
+        // Unsure if this should be true or false
+        false
     }
 }
 
-pub(super) struct RigidBodyTypeChange {
-    id: uuid::Uuid,
+#[derive(Clone)]
+pub(super) struct RigidBodyTypeState {
     body_type: RigidBodyType,
 }
 
-impl RigidBodyTypeChange {
-    pub fn new(time_tracked: &TimeTracked, body_type: RigidBodyType) -> Self {
-        Self {
-            id: time_tracked.id(),
-            body_type,
-        }
+impl RigidBodyTypeState {
+    pub fn new(body_type: RigidBodyType) -> Self {
+        Self { body_type }
     }
 }
 
-impl GameChange for RigidBodyTypeChange {
-    fn is_similar(&self, other: &Self) -> bool
+impl GameState for RigidBodyTypeState {
+    fn interpolate(&self, other: &Self, t: f32) -> Self
     where
         Self: Sized,
     {
-        self.id == other.id && self.body_type == other.body_type
+        // It's always the current state, until after t > 1
+        self.clone()
+    }
+
+    fn apply(&self, entity: &mut bevy_ecs::world::EntityMut) {
+        todo!()
+        /*
+        let rigid_body_handle = entity.get::<RapierRigidBodyHandle>().unwrap();
+
+        let mut physics_context = entity.into_world_mut().resource_mut::<PhysicsContext>();
+        let mut rigidbody = physics_context
+            .rigid_bodies
+            .get_mut(rigid_body_handle.handle)
+            .unwrap();
+        rigidbody.set_body_type(self.body_type, true); */
+    }
+
+    fn skip_during_rewind(&self) -> bool {
+        true
     }
 }
