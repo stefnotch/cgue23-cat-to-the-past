62d8cea2b7604f26cef7eb025c7518096fad39b9
stefnotch <brandmairstefan@gmail.com>
2023-04-22 22:09:28 +0200

    Move icon into windowing crate

---
+++ windowing/src/icon.rs
@@ -0,0 +1,13 @@
+use std::io::Cursor;
+
+use image::{codecs::png::PngDecoder, ImageDecoder};
+use winit::window::Icon;
+
+pub fn get_icon() -> Result<Icon, Box<dyn std::error::Error>> {
+    let decoder = PngDecoder::new(Cursor::new(include_bytes!("icon.png")))?;
+
+    let (width, height) = decoder.dimensions();
+    let mut buffer: Vec<u8> = vec![0; decoder.total_bytes() as usize];
+    decoder.read_image(&mut buffer)?;
+    Icon::from_rgba(buffer, width, height).map_err(Into::into)
+}
