f41725b77ce1e4356afac1c70e70ed9a7f892fcc
YouSafe <yelagabani@hotmail.com>
2023-06-09 11:16:23 +0200

    Start building levels

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,5 +1,7 @@
 //#![windows_subsystem = "windows"]
 
+mod levels;
+
 use animations::animation::PlayingAnimation;
 use app::entity_event::EntityEvent;
 use app::plugin::{Plugin, PluginAppAccess};
@@ -35,15 +37,13 @@ use game::player::{PlayerControllerSettings, PlayerPlugin, PlayerSpawnSettings};
 use physics::physics_context::{BoxCollider, RigidBody, RigidBodyType};
 use physics::physics_events::{CollisionEvent, CollisionEventFlags};
 
+use crate::levels::level1::Level1Plugin;
 use scene::transform::{Transform, TransformBuilder};
 
 fn spawn_world(mut commands: Commands, scene_loader: Res<SceneLoader>) {
     let before = Instant::now();
     scene_loader
-        .load_default_scene(
-            "./assets/scene/testing/prototype/prototype.glb",
-            &mut commands,
-        )
+        .load_default_scene("./assets/scene/levels/levels.gltf", &mut commands)
         .unwrap();
     println!(
         "Loading the scene took {}sec",
@@ -66,40 +66,6 @@ fn _print_fps(time: Res<Time>) {
     );
 }
 
-#[derive(Component)]
-pub struct MovingBox;
-
-pub fn spawn_moving_cube(mut commands: Commands) {
-    let cube = CpuMesh::cube(1.0, 1.0, 1.0);
-
-    commands.spawn((
-        Transform::default(),
-        Model {
-            primitives: vec![CpuPrimitive {
-                mesh: cube.clone(),
-                material: Arc::new(Default::default()),
-            }],
-        },
-        BoxCollider {
-            bounds: cube.bounding_box.clone(),
-        },
-        RigidBody(RigidBodyType::KinematicPositionBased),
-        MovingBox,
-    ));
-}
-
-pub fn move_cubes(mut query: Query<&mut Transform, With<MovingBox>>, time: Res<TimeManager>) {
-    let origin = Point3::origin();
-    for mut move_body_position in query.iter_mut() {
-        let shift = Translation3::new(
-            -4.0,
-            1.0,
-            5.0 * (time.level_time_seconds() * PI / 2.0 * 0.5).sin(),
-        );
-        move_body_position.position = shift.transform_point(&origin);
-    }
-}
-
 fn flag_system(
     mut level_flags: ResMut<LevelFlags>,
     mut game_changes: ResMut<game_change::GameChangeHistory<FlagChange>>,
@@ -119,40 +85,27 @@ fn flag_system(
     }
 }
 
-fn door_system(
-    level_flags: Res<LevelFlags>,
-    time: Res<TimeManager>,
-    mut query: Query<(&mut PlayingAnimation, &mut Door)>,
-) {
-    let door_should_open = level_flags.get(LevelId::new(0), 0);
-    let (mut animation, mut door) = query.single_mut();
-    if door_should_open && !door.is_open {
-        door.is_open = true;
-        animation.play_forwards(*time.level_time());
-    } else if !door_should_open && door.is_open {
-        door.is_open = false;
-        animation.play_backwards(*time.level_time());
-    }
-}
-
 struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&mut self, app: &mut PluginAppAccess) {
         app.with_startup_system(spawn_world)
             .with_startup_system(setup_levels)
-            .with_startup_system(spawn_moving_cube)
             .with_plugin(UIPlugin)
             .with_set(UIPlugin::system_set().in_set(AppStage::Update))
             .with_plugin(PickupPlugin)
             .with_plugin(RewindPowerPlugin)
+            .with_plugin(Level1Plugin)
+            .with_set(
+                Level1Plugin::system_set()
+                    .in_set(AppStage::Update)
+                    .after(flag_system),
+            )
             .with_set(
                 RewindPowerPlugin::system_set()
                     .in_set(AppStage::Update)
                     .before(UIPlugin::system_set()),
             )
-            .with_system(flag_system.in_set(AppStage::Update))
-            .with_system(door_system.in_set(AppStage::Update).after(flag_system))
-            .with_system(move_cubes.in_set(AppStage::Update)); //.with_system(_print_fps.in_set(AppStage::Update));
+            .with_system(flag_system.in_set(AppStage::Update));
     }
 }
 
